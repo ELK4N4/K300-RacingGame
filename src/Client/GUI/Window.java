214eb245c5b544ba11c11844ab5c81f959d05c0b
elkanagit <elkana546@gmail.com>
2019-12-19 20:14:42 +0200

    Window changes

--- src/Client/GUI/Window.java
+++ src/Client/GUI/Window.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -12,14 +13,48 @@ public class Window extends JPanel implements KeyListener {
     private static final int SCREEN_HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
     private static final int SCREEN_WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
     private static JFrame frame;
-    private BufferedImage banana;
-    private ClickThread clickThread;
+    private BufferedImage car;
+    //private ClickThread clickThread;
     static double direction = 0;
     static double x = SCREEN_WIDTH / 2.0;
     static double y = SCREEN_HEIGHT / 2.0;
     static double Cx = x;
     static double Cy = y;
 
+    private Window(){
+        //clickThread = new ClickThread(this);
+        car = getImage("Images/car_blue.png");
+        this.setBackground(Color.blue);
+        frame.addKeyListener(this);
+        //new Thread(clickThread).start();
+//        x -= car.getWidth() / 2.0;
+ //       y -= car.getHeight() / 2.0;
+    }
+
+    public static void main(String[] args) {
+        frame = new JFrame("K300");
+        JPanel panel = new Window();
+        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+/*
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        AffineTransform at = AffineTransform.getTranslateInstance(x, y);
+        g.setFont(new Font("arial", 1, 50));
+        g.setColor(Color.red);
+
+        g.drawString(direction + "", 70, 50);
+
+        at.rotate(Math.toDegrees(direction), banana.getWidth() / 2.0, banana.getHeight() / 2.0);
+
+        Graphics2D graphics2D = (Graphics2D) g;
+        graphics2D.drawImage(banana, at, null);
+    }
+*/
     private BufferedImage getImage(String imageName) {
         try {
             return ImageIO.read(getClass().getResource(imageName));
