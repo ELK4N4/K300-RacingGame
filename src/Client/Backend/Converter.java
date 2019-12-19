7369f2b4b1dcd18eeed35852d3c034e280a5227e
henyair <a0526604945@gmail.com>
2019-12-20 00:55:22 +0200

    angels

--- src/Client/Backend/Converter.java
+++ src/Client/Backend/Converter.java
@@ -1,13 +1,32 @@
 package Client.Backend;
 
 public class Converter {
-    public double convertX(double x,int width , int height)
+
+    private int width;
+    private int height;
+
+    public Converter(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX(){
+        int x;
+        x = 0;// change to get x from moshe
+        return convertX(x);
+    }
+
+    public double getY() {
+        int y;
+        y = 0;// change to get y form moshe
+        return  convertY(y);
+    }
+
+    private double convertX(double x)
     {
         return (double)(width/2 )+x;
-
-
     }
-    public double convertY(double y,int width ,int height)
+    private double convertY(double y)
     {
         return (double) (height/2)-y;
     }
