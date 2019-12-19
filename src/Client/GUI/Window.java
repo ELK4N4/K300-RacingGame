c4e5008159f8729965f21025fd40f9819d9f036f
elkanagit <elkana546@gmail.com>
2019-12-19 19:54:32 +0200

    Window changes

--- src/Client/GUI/Window.java
+++ src/Client/GUI/Window.java
@@ -1,4 +1,46 @@
 package Client.GUI;
 
-public class Window {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Window extends JPanel implements KeyListener {
+    private static final int SCREEN_HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    private static final int SCREEN_WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    private static JFrame frame;
+    private BufferedImage banana;
+    private ClickThread clickThread;
+    static double direction = 0;
+    static double x = SCREEN_WIDTH / 2.0;
+    static double y = SCREEN_HEIGHT / 2.0;
+    static double Cx = x;
+    static double Cy = y;
+
+    private BufferedImage getImage(String imageName) {
+        try {
+            return ImageIO.read(getClass().getResource(imageName));
+        } catch (IOException e) {
+            throw new Error("no path found");
+        }
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
 }
