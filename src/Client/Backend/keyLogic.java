bb9e0d469a453d06c9d3f1723e0b842fedcf0e28
Bon1bon <57953434+Bon1bon@users.noreply.github.com>
2019-12-19 23:04:44 +0200

    now for real

--- src/Client/Backend/keyLogic.java
+++ src/Client/Backend/keyLogic.java
@@ -8,108 +8,170 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.*;
 import java.io.IOException;
 
-public class keyLogic implements Runnable {
+package Testing;
 
+public class ClickThread implements Runnable {
 
-    private static final int SCREEN_HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-    private static final int SCREEN_WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-    private static JFrame frame;
-    private BufferedImage banana;
-    private ClickThread clickThread;
-    private boolean isUp = true;
-    static double direction = 0;
-    static double x = SCREEN_WIDTH / 2.0;
-    static double y = SCREEN_HEIGHT / 2.0;
-    static double Cx = x;
-    static double Cy = y;
+    private double x;
+    private double y;
+    private double Cx;
+    private double Cy;
+    private double direction;
+    private boolean up, down, right, left;
+    Test2 frame;
 
-    private Test2(){
-        clickThread = new ClickThread(this);
-        banana = getImage("Images/arrow.png");
-        this.setBackground(Color.blue);
-        frame.addKeyListener(this);
-        new Thread(clickThread).start();
-//        x -= banana.getWidth() / 2.0;
-//        y -= banana.getHeight() / 2.0;
+    public ClickThread() {
+        x =
+        y =
+        Cx = x;
+        Cy = y;
+        up = false;
+        down = false;
+        right = false;
+        left = false;
     }
 
-    private BufferedImage getImage(String imageName) {
-        try {
-            return ImageIO.read(getClass().getResource(imageName));
-        } catch (IOException e) {
-            throw new Error("no path found");
+    @Override
+    public void run() {
+        while (true) {
+            myMove();
+            //TODO repaint();
+                try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                System.out.println("error");
+                break;
+            }
         }
     }
 
-    public static void main(String[] args) {
-        frame = new JFrame("Test");
-        JPanel panel = new Test2();
-        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
-        frame.setVisible(true);
+    private void myMove() {
+        if(up) {
+            if(right) {
+                changeDirection(x, y, false);
+            }
+            if(left) {
+                changeDirection(x, y, true);
+            }
+
+            if(direction >= -0.0276 && direction <= 0.0276) {
+                calcXY(1);
+            } else if(direction >= 0.0276 && direction <= 0.0822) {
+                calcXY(-1);
+            }
+
+
+        }
+        if(down) {
+            if(right) {
+                changeDirection(x, y, false);
+            }
+            if(left) {
+                changeDirection(x, y, true);
+            }
+
+            if(direction >= -0.0276 && direction <= 0.0276) {
+                calcXYDown(1);
+            } else if(direction >= 0.0276 && direction <= 0.0822) {
+                calcXYDown(-1);
+            }
+        }
+
+
+        /*
+       if(up) {
+            if(Test2.direction >= -0.0276 && Test2.direction <= 0.0276) {
+                Test2.x += 3;
+            } else if(Test2.direction >= 0.0276 && Test2.direction <= 0.0822) {
+                Test2.x -= 3;
+            }
+            Test2.y = getY(Test2.x);
+            up = false;
+        }
+        if(down) {
+            if(Test2.direction >= -0.0276 && Test2.direction <= 0.0276) {
+                Test2.x -= 3;
+            } else if(Test2.direction >= 0.0276 && Test2.direction <= 0.0822) {
+                Test2.x += 3;
+            }
+            Test2.y = getY(Test2.x);
+            down = false;
+        }
+        if(right) {
+            changeDirection(Test2.x, Test2.y, false);
+            right = false;
+        }
+        if(left) {
+            changeDirection(Test2.x, Test2.y, true);
+            left = false;
+        }
+        */
+
     }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        AffineTransform at = AffineTransform.getTranslateInstance(x, y);
-        g.setFont(new Font("arial", 1, 50));
-        g.setColor(Color.red);
+    private double getY(double x) {
+        double y;
+        y = Math.tan(Math.toDegrees(direction)) * (x - Cx) ;
+        y = y + Test2.Cy;
+        return y;
+    }
 
-        g.drawString(Test2.direction + "", 70, 50);
+    private void calcXY(int n) {
+        double tx = n;
+        double ty = getY(tx + x);
 
-        if (isUp) {
-            at.rotate(Math.toDegrees(direction), banana.getWidth() / 2.0, banana.getHeight() / 2.0);
+        double td = Math.sqrt(Math.pow(-1, 2) + Math.pow(y - ty, 2));
+
+
+
+        x =  (x + (4 / td) * n);
+        y = y - (4 / td) *  (y - ty);
+    }
+
+    private void calcXYDown(int n) {
+        double tx = n;
+        double ty = getY(tx + x);
+
+        double td = Math.sqrt(Math.pow(-1, 2) + Math.pow(y - ty, 2));
+
+
+
+        x =  (x - (4 / td) * n);
+        y = y - (4 / td) *  (y - ty);
+    }
+
+    private void changeDirection(double x, double y, boolean left) {
+        Cx = x;
+        Cy = y;
+        if(direction >= 0.0822) {
+            direction = -0.0276;
+        }
+        else if(direction <= -0.0276) {
+            direction = 0.0822;
+        }
+        if(left) {
+            direction -= 0.0006;
+
         } else {
-            at.rotate(Math.toDegrees(direction), banana.getWidth() / 2.0, banana.getHeight() / 2.0);
-        }
-
-
-        Graphics2D graphics2D = (Graphics2D) g;
-        graphics2D.drawImage(banana, at, null);
-    }
-
-    @Override
-    public void keyTyped(KeyEvent keyEvent) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent keyEvent) {
-        if(keyEvent.getKeyCode() == KeyEvent.VK_ESCAPE) {
-            System.exit(0);
-        }
-        if(keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
-            clickThread.setRight(true);
-        }
-        if(keyEvent.getKeyCode() == KeyEvent.VK_LEFT) {
-            clickThread.setLeft(true);
-        }
-        if(keyEvent.getKeyCode() == KeyEvent.VK_UP) {
-            clickThread.setUp(true);
-            isUp = true;
-        }
-        if(keyEvent.getKeyCode() == KeyEvent.VK_DOWN) {
-            clickThread.setDown(true);
-            isUp = false;
+            direction += 0.0006;
         }
     }
 
-    @Override
-    public void keyReleased(KeyEvent keyEvent) {
-        if(keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
-            clickThread.setRight(false);
-        }
-        if(keyEvent.getKeyCode() == KeyEvent.VK_LEFT) {
-            clickThread.setLeft(false);
-        }
-        if(keyEvent.getKeyCode() == KeyEvent.VK_UP) {
-            clickThread.setUp(false);
-        }
-        if(keyEvent.getKeyCode() == KeyEvent.VK_DOWN) {
-            clickThread.setDown(false);
-        }
+    public void setUp(boolean up) {
+        this.up = up;
     }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
 }
 
