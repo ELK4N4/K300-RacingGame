ea50661b2484604f53b0942214df899cecabf19e
akivav <akiva.grobman@gmail.com>
2019-12-20 05:13:31 +0200

    added asdw

--- src/Client/Backend/BackAndForth.java
+++ src/Client/Backend/BackAndForth.java
@@ -26,6 +26,9 @@ public class BackAndForth implements  Runnable{
             main.setPlayersX(keyLogic.getX1());
             main.setPlayerY(keyLogic.getY1());
             main.setPlayerDirection(keyLogic.getDirection1());
+            main.setEnemyX(keyLogic.getX2());
+            main.setEnemyY(keyLogic.getY2());
+            main.setEnemyDirection(keyLogic.getDirection2());
             try {
                 outputStream.writeObject(new Message(keyLogic.getX1(), keyLogic.getY1(), keyLogic.getDirection1(), main.getRounds()));
             } catch (IOException e) {
