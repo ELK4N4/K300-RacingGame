c5faf2385c7a3939888e1c62f72bf1791a1c03f6
akivav <akiva.grobman@gmail.com>
2020-02-02 20:50:39 +0200

    optimizing

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -2,13 +2,10 @@ package Server;
 
 import BackandForth.CarColor;
 import BackandForth.Message;
-import Client.Backend.Car;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Server {
